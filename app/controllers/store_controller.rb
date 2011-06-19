f280af5f4ec554a49ad2dd0ea12becd377d8e3df
vinboxx <vinboxx@gmail.com>
2011-06-19 16:57:09 +0700

    Adding a Cart

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,12 @@
+class StoreController < ApplicationController
+  def index
+	@products = Product.all
+	@count = increment_count
+  end
+  
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
+
+end
